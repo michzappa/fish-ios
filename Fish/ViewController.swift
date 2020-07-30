026496d77f814c80773a06d7bf902b3a140d45e3
Michael Zappa <zapprich@gmail.com>
2020-07-30 15:06:31 -0500

    POST and DELETE working for text input

--- Fish/ViewController.swift
+++ Fish/ViewController.swift
@@ -17,23 +17,25 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    
+    // Outlets to UI Elements
     @IBOutlet weak var CreateRoomNameField: UITextField!
-    
     @IBOutlet weak var CreateRoomBtn: UIButton!
     @IBOutlet weak var DeleteRoomBtn: UIButton!
-    
     @IBOutlet weak var RoomList: UIPickerView!
+    
+    // State of the app
+    let session = URLSession.shared
     var roomNameList: [String] = [String]()
     
     var refreshTimer: Timer?
     
     override func viewDidLoad() {
+        // refresh from server
         refreshTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
             
             self.refreshRoomList()
             
-            [self.RoomList .reloadAllComponents()];
+            self.RoomList.reloadAllComponents();
         }
         
         // UIPickerView setup
@@ -45,22 +47,71 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.viewDidLoad()
     }
 
+    // Sends HTTP Post Request to server to create a room
     @objc func createRoom(){
         print("Create Button tapped")
         let roomName = self.CreateRoomNameField.text
         print(roomName!)
+        
+        let url = URL(string: "https://glistening-stale-arcticfox.gigalixirapp.com/rooms")
+        guard let requestUrl = url else { fatalError() }
+
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Powered by Swift!", forHTTPHeaderField: "X-Powered-By")
+        
+        let json: [String: Any] = ["name": "\(roomName!)"]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json, options: [])
+
+        let task = session.uploadTask(with: request, from: jsonData) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print(dataString)
+            }
+        }
+        task.resume()
+        
         self.CreateRoomNameField.text = ""
     }
     
+    // deletes room with HTTP DELETE REQUEST
     @objc func deleteRoom(){
         print("Delete Button tapped")
+        let roomName = self.CreateRoomNameField.text
+        print(roomName!)
+        
+        let url = URL(string: "https://glistening-stale-arcticfox.gigalixirapp.com/rooms/\(roomName!)")
+        guard let requestUrl = url else { fatalError() }
+        
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "DELETE"
+
+        let task = session.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print(dataString)
+            }
+        }
+        task.resume()
+        
+        self.CreateRoomNameField.text = ""
     }
     
+    // refreshes the displayed room list from the server with an HTTP GET request
     func refreshRoomList(){
         let url=URL(string: "https://glistening-stale-arcticfox.gigalixirapp.com/rooms")
         guard let requestURL = url else { fatalError() }
         
-        let task = URLSession.shared.dataTask(with: requestURL) {(data, response, error) in
+        let task = session.dataTask(with: requestURL) {(data, response, error) in
             guard let data = data else { return }
             let rooms: [Room] = try! JSONDecoder().decode([Room].self, from: data)
             //print(String(data: data, encoding: .utf8)!)
@@ -75,6 +126,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         task.resume()
     }
+    
     func numberOfComponents(in pickerView: UIPickerView)->Int{
         return 1;
     }
